488287ff5da05be6b6817deb41cde34425e8c87e
Josue Cruz <cruzlopez.josuedavid96@gmail.com>
2023-09-03 22:45:54 -0600

    tailwind CSS reusable classes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,50 @@
 import React from "react";
+import logo from "../../assets/images/logo.png";
+
+const navlinks = [
+    {
+        path: "/home",
+        display: "Home",
+    },
+    {
+        path: "/doctors",
+        display: "Find a Doctor",
+    },
+    {
+        path: "/services",
+        display: "Services",
+    },
+    {
+        path: "/home",
+        display: "Home",
+    },
+    {
+        path: "/contact",
+        display: "Contact",
+    }
+]
 
 const Header = () => {
 
-    return ( <div>Header</div> )
+    return <header className="header flex items-center"> 
+        <div className="container"> 
+            <div className="flex items-center justify-between">
+                {/* ========== logo ========== */}
+                <div>
+                    <img src={logo} alt="" />
+                </div>
+
+                {/* menu  */}
+
+                <div className="navigation">
+                    <ul className="menu flex items-center gap-[2.7rem]">
+
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </header>
 
 }
 
