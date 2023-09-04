cc55ec835d557dba6bdee5095a2be837f9f12dd9
Josue Cruz <cruzlopez.josuedavid96@gmail.com>
2023-09-04 00:42:36 -0600

    Navigation

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import { useEffect, useRef, React} from "react";
 import logo from "../../assets/images/logo.png";
+import userImg from "../../assets/images/avatar-icon.png";
+import { NavLink, Link} from "react-router-dom";
+import { BiMenu } from "react-icons/bi";
 
 const navlinks = [
     {
@@ -15,16 +18,20 @@ const navlinks = [
         display: "Services",
     },
     {
-        path: "/home",
-        display: "Home",
-    },
-    {
         path: "/contact",
         display: "Contact",
     }
 ]
 
 const Header = () => {
+
+    const headerRef = useRef(null)
+    const menuRef = useRef(null)
+    const handleStickyHeader = () => {
+        if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            headerRef.current.classList.add("sticky-header")
+        }
+    }
 
     return <header className="header flex items-center"> 
         <div className="container"> 
@@ -34,14 +41,53 @@ const Header = () => {
                     <img src={logo} alt="" />
                 </div>
 
-                {/* menu  */}
-
+                {/* ========== menu  ========== */}
                 <div className="navigation">
                     <ul className="menu flex items-center gap-[2.7rem]">
+                        {
+                            navlinks.map( (link,index) => (
+                                <li key={index}>
+                                    <NavLink    to={ link.path } 
+                                                className= { navClass =>  
+                                                    navClass.isActive
+                                                        ? "text-primaryColor text-[16px] leading-7 font-[600]"
+                                                        : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor" }>                                      
+                                        { link.display }
+                                    </NavLink>
+                                </li> ))
+                        
+                        }
 
                     </ul>
-
                 </div>
+
+                
+                { /* ========== nav right ========== */ }
+
+                <div className="flex items-center gap-4">
+                    <div className="hidden">
+                        <Link to="/">
+                            <figure className="w-[35px] h-[35px] rounded-full cursor-pointer"> 
+                                <img src={userImg} className="w-full rounded-full" alt="" />
+                            </figure>
+                        </Link>
+                    </div>
+                    <Link to="/login">
+                        <button className=" bg-primaryColor 
+                                            py-2 px-6 
+                                            text-white 
+                                            font-[600] 
+                                            h-[44px] 
+                                            flex 
+                                            items-center
+                                            justify-center 
+                                            rounded-[50px]"> Login </button>
+                    </Link>
+                    <span className="md:hidden">
+                        <BiMenu className="w-5 h-6 cursor-pointer" />
+                    </span>
+                </div>
+
             </div>
         </div>
     </header>
