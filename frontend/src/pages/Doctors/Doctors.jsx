b9f835ddfe859496eb9ebf59ad6700e6305e5b18
Josue Cruz <cruzlopez.josuedavid96@gmail.com>
2023-09-12 21:58:23 -0600

    Find a doctor

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -4,7 +4,7 @@ import DoctorCard from "./../../components/Doctors/DoctorCard";
 import Testimonial from "./../../components/Testimonial/Testimonial";
 
 const Doctors = () => {
-    
+
     return <>
         <section className="bg-[#fff9ea]">
             <div className="container text-center">
@@ -13,15 +13,15 @@ const Doctors = () => {
                 </h2>
                 <div className="max-w-[570px] mt-[30px] mx-auto bg-{#0066ff2c} rounded-md flex items-center
                 justify-between ">
-                    <input 
+                    <input
                         type="search"
                         className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer
-                        placeholder:text-textColor " 
+                        placeholder:text-textColor "
                         placeholder="Searh Doctor" />
+                    <button className="btn mt-0 rounded-[0px] rounded-r-md">
+                        Search
+                    </button>
                 </div>
-                <button className="btn mt-0 rounded-[0px] rounded-r-md">
-                    Search
-                </button>
             </div>
         </section>
 
@@ -29,8 +29,8 @@ const Doctors = () => {
             <div className="container">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ">
                     {
-                    doctors.map( doctor => (
-                        <DoctorCard key={doctors.id} doctor={doctor} />
+                        doctors.map(doctor => (
+                            <DoctorCard key={doctors.id} doctor={doctor} />
                         ))
                     }
                 </div>
