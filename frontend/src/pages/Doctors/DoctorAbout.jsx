e87d1345dab1da609c17288e34a7f2df40cd54e3
Josue Cruz <cruzlopez.josuedavid96@gmail.com>
2023-09-14 20:21:48 -0600

    Doctors feedback

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { formateDate } from "../../utils/formateData";
 
 const DoctorAbout = () => {
@@ -28,29 +27,62 @@ const DoctorAbout = () => {
                     <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb[30px] ">
                         <div>
                             <span className="text-irisBlueColor text-[15px] leading-6 font-semibold ">
-                                { formateDate("12-04-2010") }
+                                { formateDate("09-04-2014") } - { formateDate("09-13-2016") }
                             </span>
                             <p className="text-[16px] leading-6 font-medium text-textColor ">
-                                PHP in Surgeoun
+                                PHD in Surgeoun
                             </p>
                         </div>
                         <p className="text-[14px] leading-5 font-medium text-textColor ">
-                            New Apollo Hospital, New York.
+                            Apollo Hospital, New York.
                         </p>
                     </li>
-                    <li>
+                    <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb[30px] ">
                         <div>
                             <span className="text-irisBlueColor text-[15px] leading-6 font-semibold ">
-                                { formateDate("12-04-2010") }
+                                { formateDate("7-04-2010") } - { formateDate("09-04-2014") }
                             </span>
                             <p className="text-[16px] leading-6 font-medium text-textColor ">
-                                PHP in Surgeoun
+                                PHD in Surgeoun
                             </p>
                         </div>
                         <p className="text-[14px] leading-5 font-medium text-textColor ">
-                            New Apollo Hospital, New York.
+                            Apollo Hospital, New York.
                         </p>
 
+                    </li>
+                </ul>
+            </div>
+
+            <div className="mt-12">
+                <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold ">
+                    Experience
+                </h3>
+
+                <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5 ">
+                    <li className="p-4 rounded bg-[fff9ea] ">
+                        <span className="text-yellowColor text-[15px] leading-6 font-semibold ">
+                            { formateDate("07-04-2010") } - { formateDate("08-13-2014") }
+                        </span>
+                        <p className="text-[16px] leading-5 font-medium text-textColor ">
+                            Sr. Surgeon
+                        </p>
+
+                        <p className="text-[14px] leading-5 font-medium text-textColor ">
+                            New Apollo Hospital, New York
+                        </p>
+                    </li>
+                    <li className="p-4 rounded bg-[fff9ea] ">
+                        <span className="text-yellowColor text-[15px] leading-6 font-semibold ">
+                            { formateDate("07-04-2010") } - { formateDate("08-13-2014") }
+                        </span>
+                        <p className="text-[16px] leading-5 font-medium text-textColor ">
+                            Sr. Surgeon
+                        </p>
+
+                        <p className="text-[14px] leading-5 font-medium text-textColor ">
+                            New Apollo Hospital, New York
+                        </p>
                     </li>
                 </ul>
             </div>
